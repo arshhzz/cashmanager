fb81f99e1e7485848bfd29df485e6898414d1ded
arsh <arshdeepkhalsa60@gmail.com>
2025-05-31 17:27:46 +0530

    :# Please enter the commit message for your changes. Lines starting

--- cashmanager/frontend/src/pages/signin.jsx
+++ cashmanager/frontend/src/pages/signin.jsx
@@ -1,5 +1,29 @@
-
+import { BottomWarning } from "../components/BottomWarning"
+import { Button } from "../components/Button"
+import { Heading } from "../components/Heading"
+import { InputBox } from "../components/InputBox"
+import { SubHeading } from "../components/SubHeading"
 
 export const Signin = () => {
-  
+
+  return <div className="bg-slate-300 h-screen flex justify-center">
+    <div
+      className="h-screen w-full bg-gradient-to-r from-[#40e0d0] to-[#008080] bg-[length:200%_200%] flex justify-center items-center px-4"
+      style={{
+        animation: 'gradientShift 8s ease infinite',
+        backgroundSize: '200% 200%',
+      }}
+    >
+      <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+        <Heading label={"Sign in"} />
+        <SubHeading label={"Enter your credentials to access your account"} />
+        <InputBox placeholder="your@email.com" label={"Email"} />
+        <InputBox placeholder="••••••••" label={"Password"} />
+        <div className="pt-4">
+          <Button label={"Sign in"} />
+        </div>
+        <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
+      </div>
+    </div>
+  </div>
 }
--- cashmanager/frontend/src/pages/signup.jsx
+++ cashmanager/frontend/src/pages/signup.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react"
 import { BottomWarning } from "../components/BottomWarning"
 import { Button } from "../components/Button"
 import { Heading } from "../components/Heading"
 import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
+import axios from "axios";
+
 export const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div
       className="h-screen w-full bg-gradient-to-r from-[#40e0d0] to-[#008080] bg-[length:200%_200%] flex justify-center items-center px-4"
@@ -16,19 +23,30 @@ export const Signup = () => {
         <Heading label="Sign Up" />
         <SubHeading label="Join us and take control of your money" />
         <div className="space-y-4 text-left">
-          <InputBox placeholder="Enter your first name" label="First Name" />
-          <InputBox placeholder="Enter your last name" label="Last Name" />
-          <InputBox placeholder="you@example.com" label="Email" />
-          <InputBox placeholder="••••••••" label="Password" />
-        </div>
+          <InputBox onChange={(e) => {
+            setFirstName(e.target.value)
+          }} placeholder="Enter your first name" label="First Name" />
+          <InputBox onChange={(e) => {
+            setLastName(e.target.value)
+          }} placeholder="Enter your last name" label="Last Name" />
+          <InputBox onChange={(e) => {
+            setUsername(e.target.value)
+          }} placeholder="you@example.com" label="Email" />
+          <InputBox onChange={(e) => {
+            setPassword(e.target.value)
+          }} placeholder="••••••••" label="Password" />
+        </div>  
         <div className="pt-2">
-          <Button label="Continue" />
+          <Button onClick={() =>{
+            axios.post("http://localhost:3000/api/v1/user/signup", {
+              username,
+              firstName,
+              password,
+              lastName
+            })
+          }} label="Continue" />
         </div>
-        <BottomWarning
-          label="Already using the app?"
-          buttonText="Sign in"
-          to="/signin"
-        />
+        <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
       </div>
     </div>
   );
